df813649f4e1fccdd8e8e8486d27ede526804d8e
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-09-17 10:41:42 +0300

    fix: change routes order

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,9 @@ const {
 } = require('../controllers/users');
 
 router.get('/users', getUsers);
-router.get('/users/:userId', getUserById);
-router.post('/users', createUser);
 router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);
+router.get('/users/:userId', getUserById);
+router.post('/users', createUser);
 
 module.exports = router;
