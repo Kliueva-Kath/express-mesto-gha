614d53773abcc77bfcd5628a1c7ccb5fb94af350
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-09-19 19:12:14 +0300

    feat: add password & email to User Schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,6 +17,22 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Пожалуйста, проверьте правильность ввода',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 });
 
