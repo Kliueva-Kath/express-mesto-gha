4ea19992666a8bdac84c2befbe7ddc24af5a00ef
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-09-29 13:34:26 +0300

    feat: set up error logging with Winston

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { login, createUser } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
 const { error } = require('./middlewares/error');
 const { urlRegExp } = require('./utils/regExp');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
@@ -33,7 +34,14 @@ main();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(requestLogger);
 app.use(cors);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -60,5 +68,6 @@ app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(error);
